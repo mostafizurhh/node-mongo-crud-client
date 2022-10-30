10815e9ba37c0fb087282239aaf127229f55dc98
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-30 11:40:54 +0000

    handlesubmit function modified to update user in server and DB as well as show on UI

--- src/components/Update.js
+++ src/components/Update.js
@@ -3,10 +3,27 @@ import { useLoaderData } from 'react-router-dom';
 
 const Update = () => {
     const storedUser = useLoaderData()
+
     const [user, setuser] = useState(storedUser)
+
     const handleSubmit = event => {
         event.preventDefault();
         console.log(user)
+        fetch(`http://localhost:5000/users/${storedUser._id}`, {
+            method: 'put',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    alert('user updated successfully')
+                    console.log(data)
+                }
+            })
+            .catch(error => console.error(error))
     }
 
     const handleInputChange = event => {
