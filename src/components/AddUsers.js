716195a5bfbade7e1b8d2b1ae6ebd7e17cdfad07
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-30 01:13:04 +0000

    CREATE, READ AND DELETE OPERATION EXPLORED

---
+++ src/components/AddUsers.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useState } from 'react';
+
+const AddUsers = () => {
+    const [user, setuser] = useState({})
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(user)
+
+        /* send data to server side */
+        fetch('http://localhost:5000/users', {
+            method: 'post',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    alert('data inserted successfully');
+                    event.target.reset();
+                }
+                console.log(data)
+            })
+            .catch(error => console.error(error))
+    }
+
+    const handleInputBlur = event => {
+        const field = event.target.name
+        const value = event.target.value
+        const newUser = { ...user }
+        newUser[field] = value
+        setuser(newUser)
+    }
+
+    return (
+        <div>
+            <h2>This is User</h2>
+            <form onSubmit={handleSubmit}>
+                <input onBlur={handleInputBlur} type="text" name='name' placeholder='your name' /> <br /> <br />
+                <input onBlur={handleInputBlur} type="text" name='address' placeholder='your address' /> <br /> <br />
+                <input onBlur={handleInputBlur} type="email" name='email' placeholder='your email' /> <br /> <br />
+                <button type='submit'>Add User</button>
+            </form>
+        </div>
+    );
+};
+
+export default AddUsers;
---
+++ src/components/Home.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Home = () => {
+    /* display data to UI from DB/server API */
+    const users = useLoaderData()
+    const [displayUsers, setDisplayUsers] = useState(users)
+
+    /* delete a data from client side, server side and DB. and update UI instantly */
+    const handleDlete = user => {
+        const agree = window.confirm(`Are you sure you want to delete ${user.name}`)
+        if (agree) {
+            // console.log('deleting user with id', user._id)
+            fetch(`http://localhost:5000/users/${user._id}`, {
+                method: 'delete'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('user deleted successfully');
+                        const remaining = displayUsers.filter(usr => usr._id !== user._id);
+                        setDisplayUsers(remaining);
+                    }
+                })
+                .catch(error => console.error(error))
+        }
+    }
+
+    return (
+        <div>
+            <h2>Users: {displayUsers.length}</h2>
+            <div>
+                {
+                    displayUsers.map(user => <p key={user._id}>
+                        {user.name}: {user.email} <button onClick={() => handleDlete(user)}><small>x</small></button>
+                    </p>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Home;
